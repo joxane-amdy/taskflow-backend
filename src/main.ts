edf6dc05d25f5c5d2d591e25863bc9a60ad2296f
Amdy Joxane <amdymoustaphadiokhane@gmail.com>
2026-07-04 20:20:28 +0000

    feat: CORS GitHub Pages + ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://joxane-amdy.github.io',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
