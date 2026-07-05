34d1942000eac895ed8d24bbf53c24118f62fb5b
Amdy Joxane <amdymoustaphadiokhane@gmail.com>
2026-07-05 19:32:09 +0000

    fix: ajout champ role dans CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsEnum, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -13,4 +13,8 @@ export class CreateUserDto {
   @IsString()
   @MinLength(6, { message: 'Le mot de passe doit faire au moins 6 caractères' })
   password: string;
+
+  @IsOptional()
+  @IsEnum(['admin', 'user'], { message: 'role doit être "admin" ou "user"' })
+  role?: string;
 }
